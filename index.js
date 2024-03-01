db2194a5a96b8d17aea6a04b18e2441904ea8ffc
Rudraksh Joshi <rudrakshjoshi27@gmail.com>
2024-03-01 12:55:17 +0530

    multiple promises handling

--- index.js
+++ index.js
@@ -88,7 +88,7 @@ const superagent = require('superagent');
 
 function readFilePro(filePath) {
   const myPromise = new Promise((resolve, reject) => {
-    console.log(filePath);
+    // console.log(filePath);
     // executor function which contains the main logic of async operation
     fs.readFile(filePath, 'utf-8', (err, data) => {
       if (err) reject('Alas! file could not be found ❌');
@@ -154,6 +154,7 @@ readFilePro(`${__dirname}/dog.txt`)
 ////////////////////////////////////////////////////////////////////////////////////////////
 // consuming promises with async await
 // allows us to make our code look more synchronous
+/*
 async function getDogPic() {
   try {
     const data = await readFilePro(`${__dirname}/dog.txt`);
@@ -165,10 +166,94 @@ async function getDogPic() {
     const { message } = res.body;
     // console.log(message)
     // return message;
-    await writeFilePro(`${__dirname}/dog-img-url.txt`, message);
-    console.log('written image url to file.....');
+    const written = await writeFilePro(`${__dirname}/dog-img-url.txt`, message);
+    console.log(written);
+    // console.log('written image url to file.....');
   } catch (err) {
-    console.log(err);
+    throw err;
+    // console.log(err);
   }
+  // even if there was an error it would still return a resolved promise hence
+  // throw error then only it will go in catch block as rejected promise
+  return `2 : READY`;
+}*/
+// console.log('1 : getting dog pic...........');
+// const res = getDogPic();
+// console.log(res); // async fn automatically returns a promise here it is in pending state
+// console.log('3 : done getting dog pic..........');
+
+// once the promise is resolved it returns the data
+// problem is here we are mixing async await with then and catch
+/*
+res
+  .then((data) => {
+    console.log(data);
+    console.log('3 : done getting dog pic..........');
+  })
+  .catch((err) => {
+    console.log('in error');
+    console.log(err);
+  });
+*/
+// another way would be to use async await
+const res2 = async () => await getDogPic();
+// console.log(res2);
+
+// using IIFE
+/*
+(async () => {
+  try {
+    console.log('1: will get dog pics !');
+    const s2 = await getDogPic();
+    console.log(s2);
+    console.log('3: done getting dog pics !');
+  } catch (error) {
+    console.log('ERROR ');
+  }
+})();
+*/
+
+/////////////////////////////////////////
+// Waiting for  multiple promises simultaneously
+
+async function getDogPic() {
+  try {
+    const data = await readFilePro(`${__dirname}/dog.txt`);
+    console.log(data);
+    /*
+    const res1 = await superagent.get(
+      `https://dog.ceo/api/breed/${data}/images/random`
+    );
+    const res2 = await superagent.get(
+      `https://dog.ceo/api/breed/${data}/images/random`
+    );
+    const res3 = await superagent.get(
+      `https://dog.ceo/api/breed/${data}/images/random`
+    );*/
+    // but why wait for once promise cause of another promise this could add unnecessary waiting time
+
+    const res1Pro = superagent.get(
+      `https://dog.ceo/api/breed/${data}/images/random`
+    );
+    const res2Pro = superagent.get(
+      `https://dog.ceo/api/breed/${data}/images/random`
+    );
+    const res3Pro = superagent.get(
+      `https://dog.ceo/api/breed/${data}/images/random`
+    );
+
+    const all = await Promise.all([res1Pro, res2Pro, res3Pro]);
+    // Promise.any returns the first only resolved promise , rejected if empty input
+    const imgs = all.map((promise) => promise.body.message);
+    console.log(imgs);
+    const written = await writeFilePro(
+      `${__dirname}/dog-img-url.txt`,
+      imgs.join('\n')
+    );
+    console.log(written);
+  } catch (err) {
+    throw err;
+  }
+  return `2 : READY`;
 }
 getDogPic();
